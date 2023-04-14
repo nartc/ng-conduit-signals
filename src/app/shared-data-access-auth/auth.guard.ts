9d9b478100efedcbfdae78c06df2c24e6d4f144e
Chau Tran <chautran@Chaus-MacBook-Pro.local>
2023-04-14 10:01:13 -0500

    update angular rc1

--- src/app/shared-data-access-auth/auth.guard.ts
+++ src/app/shared-data-access-auth/auth.guard.ts
@@ -1,27 +1,15 @@
-import { DestroyRef, effect, inject } from '@angular/core';
-// import { toObservable } from '@angular/core/rxjs-interop';
+import { inject } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
 import { CanMatchFn, Router } from '@angular/router';
-import { ReplaySubject, filter, map } from 'rxjs';
+import { filter, map } from 'rxjs';
 import { AuthService } from './auth.service';
 
 export function authGuard(type: 'protected' | 'unprotected'): CanMatchFn {
-    const sub = new ReplaySubject<boolean>(1);
     return () => {
         const router = inject(Router);
         const authService = inject(AuthService);
-        const destroyRef = inject(DestroyRef);
 
-        const watcher = effect(() => {
-            sub.next(authService.isAuthenticated());
-        });
-
-        destroyRef.onDestroy(() => {
-            sub.complete();
-            watcher.destroy();
-        });
-
-        // until the build error is fixed, use this instead
-        return sub.pipe(
+        return toObservable(authService.isAuthenticated).pipe(
             filter(() => !authService.isAuthenticating()),
             map((isAuthenticated) => {
                 if ((type === 'unprotected' && !isAuthenticated) || (type === 'protected' && isAuthenticated))
